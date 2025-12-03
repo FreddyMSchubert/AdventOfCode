ecc1f8eb2456c8db6fb70570c2d1cd905f1a85a9
FreddyMSchubert <github@frederickschubert.de>
2025-12-03 20:54:28 +0000

    2025 Day 3 Part 2

--- 2025/Day03/src/Main.java
+++ 2025/Day03/src/Main.java
@@ -1,17 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
-
-/*
-
-119811
-118911
-999997
-
-pick largest for 10 place, unless at the very end, then pick second largest or just earlier occurence of same digit
-pick largest to the right of that for 1 place
-
- */
 
 public class Main
 {
@@ -26,12 +16,15 @@ public class Main
 
     public static void main(String[] args)
     {
-        int joltsTracker = 0;
+        int twoDigitJoltsTracker = 0;
+        BigInteger twelveDigitJoltsTracker = BigInteger.ZERO;
 
         try (BufferedReader br = new BufferedReader(new FileReader("./src/input.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 ArrayList<Integer> battery = splitBattery(line);
+
+                // ----- 2 - digit jolts
 
                 // 1. find largest digit not at the very end
                 int tenPlace = 0;
@@ -47,12 +40,38 @@ public class Main
                         onePlace = i;
                 }
 
-                System.out.println("Battery " + line + " results in largest possible jolts " + battery.get(tenPlace) + battery.get(onePlace) + ".");
+                System.out.println("Battery " + line + " results in largest possible 2 - digit jolts " + battery.get(tenPlace) + battery.get(onePlace) + ".");
 
-                joltsTracker += battery.get(tenPlace) * 10 + battery.get(onePlace);
+                twoDigitJoltsTracker += battery.get(tenPlace) * 10 + battery.get(onePlace);
+
+                // ----- 12 - digit jolts
+
+                Integer[] twelveDigits = new Integer[12];
+                for (int i = 0; i < 12; i++)
+                    twelveDigits[i] = 0;
+
+                for (int currDigit = 0; currDigit < 12; currDigit++) {
+                    int prevDigit = currDigit == 0 ? -1 : twelveDigits[currDigit - 1];
+                    for (int i = prevDigit + 1; i < battery.size() - 12 + currDigit + 1; i++) {
+                        if (battery.get(i) > battery.get(twelveDigits[currDigit]) || i == prevDigit + 1)
+                            twelveDigits[currDigit] = i;
+                    }
+                }
+
+                BigInteger twelveMax = BigInteger.ZERO;
+                BigInteger currentMult = BigInteger.ONE;
+                for (int i = 11; i >= 0; i--) {
+                    twelveMax = twelveMax.add(BigInteger.valueOf(battery.get(twelveDigits[i])).multiply(currentMult));
+                    currentMult = currentMult.multiply(BigInteger.TEN);
+                }
+
+                System.out.println("Battery " + line + " results in largest possible 12 - digit jolts " + twelveMax + ".");
+
+                twelveDigitJoltsTracker = twelveDigitJoltsTracker.add(twelveMax);
             }
 
-            System.out.println("For all batteries the largest possible jolts are " + joltsTracker + ". :D");
+            System.out.println("For all batteries the largest possible 2 - digit jolts are " + twoDigitJoltsTracker + ". :D");
+            System.out.println("For all batteries the largest possible 12 - digit jolts are " + twelveDigitJoltsTracker + ". :P");
         }
         catch (Exception e)
         {
