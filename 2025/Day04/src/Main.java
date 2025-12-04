941a34d5460b7594184cd1632ebeb67fce67f9c6
FreddyMSchubert <github@frederickschubert.de>
2025-12-04 14:32:08 +0000

    2025 Day 4 Part 1

---
+++ 2025/Day04/src/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+..xx.xx@x.
+x@@.@.@.@@
+@@@@@.x.@@
+@.@@@@..@.
+x@.@@@@.@x
+.@@@@@@@.@
+.@.@.@.@@@
+x.@@@.@@@@
+.@@@@@@@@.
+x.x.@@@.x.
+
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader("./src/input.txt"))) {
+            String line;
+            List<String> map = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                map.add(line);
+            }
+
+            int totalAccessibleRolls = 0;
+
+            for (int y = 0; y < map.size(); y++) {
+                for (int x = 0; x < map.get(y).length(); x++) {
+                    if (map.get(y).charAt(x) != '@')
+                        continue;
+
+                    int rollsCount = 0;
+                    for (int dy = -1; dy <= 1; dy++) {
+                        for (int dx = -1; dx <= 1; dx++) {
+                            int localY = y + dy;
+                            int localX = x + dx;
+                            if (localY < 0 || localY >= map.size() || localX < 0 || localX >= map.get(localY).length())
+                                continue;
+                            if (localY == y && localX == x)
+                                continue;
+                            if (map.get(localY).charAt(localX) == '@')
+                                rollsCount++;
+                        }
+                    }
+                    if (rollsCount < 4)
+                    {
+                        totalAccessibleRolls++;
+                        System.out.println("Accessible roll found at [" + x + "|" + y + "].");
+                    }
+                }
+            }
+
+            System.out.println("Total accessible rolls are: " + totalAccessibleRolls + ". :P");
+        }
+        catch (Exception e)
+        {
+            System.out.println("oof" + e);
+        }
+    }
+}
