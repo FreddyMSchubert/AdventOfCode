c6ca780fb3b8c7eda86a1d88b36e250f631f8a88
FreddyMSchubert <github@frederickschubert.de>
2025-12-05 11:21:45 +0000

    2025 Day 5 Part 2

--- 2025/Day05/src/Main.java
+++ 2025/Day05/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main
@@ -29,6 +30,8 @@ public class Main
             System.out.println("oof" + e);
         }
 
+        // PART 1
+
         int freshIngredientCount = 0;
 
         for (Long ingredientId : ingredientIds) {
@@ -41,6 +44,39 @@ public class Main
         }
 
         System.out.println("Found " + freshIngredientCount + " fresh ingredients. :)");
+
+        // PART 2
+
+        // create a sanitized range pair array first
+        // for each range to be added, check existing ranges.
+        // if overlap, remove that range and incorporate it into current range
+
+        List<Pair<Long, Long>> sanitizedRanges = new ArrayList<>();
+
+        for (Pair<Long, Long> rangeToAdd : ranges) {
+            Iterator<Pair<Long, Long>> it = sanitizedRanges.iterator();
+            while (it.hasNext()) {
+                Pair<Long, Long> rangeToCompare = it.next();
+
+                if (rangeToAdd.first() <= rangeToCompare.second() && rangeToAdd.second() >= rangeToCompare.first())
+                {
+                    System.out.print("Detected an overlap between ranges " + rangeToAdd.first() + "-" + rangeToAdd.second() + " and " + rangeToCompare.first() + "-" + rangeToCompare.second() + ", turning that into range ");
+                    rangeToAdd = new Pair<>(Long.min(rangeToAdd.first(), rangeToCompare.first()), Long.max(rangeToAdd.second(), rangeToCompare.second()));
+                    System.out.println(rangeToAdd.first() + "-" + rangeToAdd.second() + ". :)");
+                    it.remove();
+                    it = sanitizedRanges.iterator();
+                }
+            }
+
+            sanitizedRanges.add(rangeToAdd);
+        }
+
+        long totalUnspoiledIngredientIds = 0;
+        for (Pair<Long, Long> sanitizedRange : sanitizedRanges) {
+            totalUnspoiledIngredientIds += sanitizedRange.second() - sanitizedRange.first() + 1;
+            System.out.println("Found sanitized range " + sanitizedRange.first() + "-" + sanitizedRange.second() + ".");
+        }
+        System.out.println("Total unspoiled ingredient IDs: " + totalUnspoiledIngredientIds + ".");
     }
 }
 record Pair<A, B>(A first, B second) {}
