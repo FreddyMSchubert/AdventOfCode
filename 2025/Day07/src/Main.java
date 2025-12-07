fd9896a568c71161e3a6cd1701623d04f33c5c71
FreddyMSchubert <github@frederickschubert.de>
2025-12-07 10:47:38 +0000

    2025 Day 7 Part 2

--- 2025/Day07/src/Main.java
+++ 2025/Day07/src/Main.java
@@ -4,26 +4,7 @@ import java.util.*;
 
 /*
 
-.......S.......
-.......|.......
-......|^|......
-......|.|......
-.....|^|^|.....
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............
-
-1. find the s
-2. then maintain a list of beam location x values
-3. loop through, progressing all beams before going one down vertically
+1590 too low
 
  */
 
@@ -67,5 +48,33 @@ public class Main
         }
 
         System.out.println("Beam Splits: " + beamSplits);
+
+        // PART 2
+        HashMap<Long, Long> memo = new HashMap<>(); // y * maxX + x, result
+        long timelines = getTimelinesOriginatingFrom(map, map.getFirst().indexOf('S'), 0, memo);
+        System.out.println("Found " + timelines + " possible tachyon timelines.");
+    }
+
+    // stack overflow incoming
+    // yes took way too long doing memoization i guess
+    // memoization is so op :)
+    public static long getTimelinesOriginatingFrom(ArrayList<String> map, long x, long y, HashMap<Long, Long> memo) {
+        if (memo.containsKey(y * map.getFirst().length() + x))
+            return memo.get(y * map.getFirst().length() + x);
+
+        long originalY = y;
+
+        do {
+            y++;
+            if (memo.containsKey(y * map.getFirst().length() + x))
+                return memo.get(y * map.getFirst().length() + x);
+            if (y >= map.size())
+                return 1;
+        }
+        while (map.get((int)y).charAt((int)x) != '^');
+
+        long timelinesOriginating = getTimelinesOriginatingFrom(map, x - 1, y, memo) + getTimelinesOriginatingFrom(map, x + 1, y, memo);
+        memo.put(originalY * map.getFirst().length() + x, timelinesOriginating);
+        return timelinesOriginating;
     }
 }
