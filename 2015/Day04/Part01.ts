134d75d2c56c689d5d4ab142ebd12f57bfdad688
FreddyMSchubert <github@frederickschubert.de>
2026-06-06 14:15:57 +0200

    2015 Day 4

---
+++ 2015/Day04/Part01.ts
@@ -0,0 +1,11 @@
+// https://gist.github.com/basarat/90cf19f5f13db6213fcb1d1292f4e931
+import * as crypto from 'crypto';
+export const md5 = (contents: string) => crypto.createHash('md5').update(contents).digest("hex");
+
+for (let i = 0; ; i++) {
+	const hash = md5("iwrupvqb" + i);
+	if (hash.startsWith("00000")) {
+		console.log(i);
+		break;
+	}
+}
